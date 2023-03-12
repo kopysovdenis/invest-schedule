234e66ed3b9c695e14dce1f644da9fc880777ab0
Kopysov Denis <kopysovdenisalexeevich@gmail.com>
2023-03-12 20:47:53 +0400

    Initial commit

---
+++ src/main/java/da/kopysov/investschedule/InvestScheduleApplication.java
@@ -0,0 +1,13 @@
+package da.kopysov.investschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/da/kopysov/investschedule/InvestScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package da.kopysov.investschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
